aa84b5b050bf067a99ba4e767544f414bfce6d7d
HackerDev <tejasimha222@gmail.com>
2020-08-30 20:42:28 +0530

    Changed response code to 200

--- controllers/member.js
+++ controllers/member.js
@@ -8,7 +8,7 @@ exports.checkMember = (req, res, next) => {
       if (member) {
         res.status(200).json({ ok: "1" });
       } else {
-        res.status(404).json({ ok: "0" });
+        res.status(200).json({ ok: "0" });
       }
     })
     .catch((err) => console.log(err));
