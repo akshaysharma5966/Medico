4cfb604454731c8b0c7d332d0b648d82ef1ac635
HackerDev <tejasimha222@gmail.com>
2020-09-02 13:55:09 +0530

    Added pagination for Medicines

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -1,11 +1,16 @@
 const Medicine = require("../models/medicine");
 
+const ITEMS_PER_PAGE = 15;
+
 exports.getMedicines = (req, res, next) => {
-	Medicine.find()
-		.then((medicines) => {
-			res.status(200).json(medicines);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  const page = req.query.page;
+  Medicine.find()
+    .skip(ITEMS_PER_PAGE * page)
+    .limit(ITEMS_PER_PAGE)
+    .then((medicines) => {
+      res.status(200).json(medicines);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
